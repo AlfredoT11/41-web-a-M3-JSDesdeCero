44f5971f314a3d6caffd270142f544f134b57f70
Tonatiuh Díaz <alfredo117tdg@hotmail.com>
2025-03-25 20:07:30 -0600

    Se agrega ejemplo de ciclos y arreglos

---
+++ 3.2.1_CiclosYArreglos/script.js
@@ -0,0 +1,114 @@
+let contador = 1;
+
+// while -> Es una estructura de control que nos permite repetir una serie de 
+// instrucciones.
+/*
+    while (condicion){
+        // Código a ejecutar
+    }
+
+Nota: La condición SIEMPRE se ejecuta primero.
+
+Generalmente utilizamos un ciclo while cuando no sabemos con exactitud
+en qué momento se dejará de cumplir la condición.
+*/
+console.log("Ejecución de un while");
+while(contador <= 10){
+    console.log(contador);
+    contador++;
+}
+
+// contador = 10;
+
+// do-while -> Es una estructura muy parecida al while, sin embargo,
+// aquí el código se ejecuta SIEMPRE al menos una vez.
+/*
+    do{
+        // Código a ejecutar.
+    }while(condicion);
+*/
+console.log("Ejecución de un do-while");
+do{
+    console.log("Soy el código de un do-while");
+}while(contador < 10);
+
+/*
+
+for(inialización; condición; operación de acumulación){
+    // Bloque a ejecutar.
+}
+
+¿Será posible inicializar más de una variable y modificar más de una variable
+en la acumulación?
+
+Generalmente utilizamos un ciclo for cuando estamos seguros del número
+de iteraciones a ejecutar
+*/
+console.log("Ejecución de un for");
+for(let i = 0; i <= 20; i += 2){
+    console.log(i);
+}
+
+let nombre = "Abril";
+let nombre2 = "Alexis";
+let nombre3 = "Luis";
+let nombre4 = "Jeshua";
+
+
+// let alumnosTona = []; // Arreglo vacío.
+// let alumnosTona = Array(); // Arreglo vacío.
+//let alumnosTona = Array(5); // Un arreglo con 5 posiciones. Estas posiciones NO son finales.
+let alumnosTona = ["Abril", "Alexis", "Luis", "Jeshua", "Jaqueline", "Victor"];
+
+console.log(typeof alumnosTona); // typeof es un operador que nos permite conocer el tipo de dato de un valor o una variable.
+console.log(alumnosTona);
+
+// Índice de un arreglo -> Es la posición de un valor como si fuera una coordenada.
+/*
+    El índice empieza a contarse desde 0.
+      0        1         2       3         4            5
+    ["Abril", "Alexis", "Luis", "Jeshua", "Jaqueline", "Victor"]
+
+*/
+
+console.log(alumnosTona[1]); // Lectura de un valor de un arreglo.
+alumnosTona[1] = "Mendez"; // Modificando un valor del arreglo.
+console.log(alumnosTona[1]);
+
+// console.log(alumnosTona[-1]); NO podemos utilizar un valor negativo como índice.
+
+// En JS una arreglo puede tener cualquier tipo de dato.
+// No está restringido a que todos los datos de este sean del mismo tipo.
+let arregloTutifruti = ["Soy una cadena", 1.5, 2, "Soy otra cadena", null, undefined, [1, 2, 3, [1, 2, 3]], "Una última cadena"];
+
+// Métodos (acciones) de los arreglos.
+let frutas = [];
+frutas.push('Naranja'); // Agrega elementos al final del arreglo.
+console.log(frutas);
+frutas.push('Manzana');
+console.log(frutas);
+
+frutas.unshift('Uva'); // Agregar elementos al inicio del arreglo.
+console.log(frutas);
+
+frutas.push('Zanahoria');
+frutas.unshift('Cebolla');
+console.log(frutas);
+
+frutas.pop(); // Elimina el último elemento del arreglo.
+console.log(frutas);
+
+frutas.shift(); // Elimina el primer elemento del arreglo.
+console.log(frutas);
+
+
+// ¿Cómo eliminar un valor que no está al inicio ni al final?
+frutas.splice(1, 1, 'Fresa', 'Mango'); // Nos permite eliminar y agregar nuevos valores.
+console.log(frutas);
+
+// ¿Cómo acceder a más de un elemento de un arreglo?
+//     0        1        2          3
+// ['Uva', 'Fresa', 'Mango', 'Manzana']
+console.log(frutas.slice(1, 3)); // Nos permite acceder a un subarreglo.
+
+
