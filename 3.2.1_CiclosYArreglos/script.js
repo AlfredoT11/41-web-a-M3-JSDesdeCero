32e56263451d8afefc709d6ae80313ed4bf89bad
Tonatiuh Díaz <alfredo117tdg@hotmail.com>
2025-03-25 20:46:39 -0600

    Se agrega información para el ejercicio de campus

--- 3.2.1_CiclosYArreglos/script.js
+++ 3.2.1_CiclosYArreglos/script.js
@@ -135,3 +135,45 @@ console.log(pares);
 
 console.log("Impares: ");
 console.log(impares);
+
+// Datos para el ejercicio: 
+// Objetos -> Objeto literal -> Diccionario
+// Estructura clave-valor.
+let fruta1 = {
+    nombre: "Naranja",
+    tipo: "Cítrico"
+}
+
+let fruta2 = {
+    nombre: "Manzana",
+    tipo: "Pomácea"
+}
+
+
+let frutasObjetos = [fruta1, fruta2];
+console.log(frutasObjetos[0].tipo); // Accediendo a las propiedades de un objeto literal.
+
+let frutasConTipo = [
+    { nombre: "Naranja", tipo: "Cítrico" },
+    { nombre: "Manzana", tipo: "Pomácea" },
+    { nombre: "Plátano", tipo: "Tropical" },
+    { nombre: "Uva", tipo: "Baya" },
+    { nombre: "Fresa", tipo: "Baya" },
+    { nombre: "Piña", tipo: "Tropical" },
+    { nombre: "Mango", tipo: "Tropical" },
+    { nombre: "Cereza", tipo: "Drupa" },
+    { nombre: "Pera", tipo: "Pomácea" },
+    { nombre: "Sandía", tipo: "Cucurbitácea" },
+    { nombre: "Melón", tipo: "Cucurbitácea" },
+    { nombre: "Durazno", tipo: "Drupa" },
+    { nombre: "Coco", tipo: "Drupa" },
+    { nombre: "Kiwi", tipo: "Baya" },
+    { nombre: "Limón", tipo: "Cítrico" }
+];
+
+
+let citricos;
+let pomaceas;
+let tropicales;
+let drupas;
+let cucurbitaceas;
