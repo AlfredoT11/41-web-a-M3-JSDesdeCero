bd00d7a3e07434ddb1d746b538b557228af079e0
Tonatiuh Díaz <alfredo117tdg@hotmail.com>
2025-03-27 21:01:33 -0600

    Se agregan ejercicios complementarios

--- 3.2.2_Funciones/script.js
+++ 3.2.2_Funciones/script.js
@@ -192,3 +192,23 @@ console.log("Fuera de la función: " + nombreGato);
 let persona1 = null; // JS va a reservar espacio en memoria para guardar ahí un valor.
 let persona2 = undefined; // JS ni siquiera reserva espacio para guardar algo.
 
+// Ejercicios adicionales
+/*
+    Fácil
+    1. Modificar la función de agregarLibro para evitar agregar duplicados.
+    2. Crear una función para buscar un libro por nombre. En caso de que exista, regresar true, caso contario regresar false.
+    3. Crear una función que regrese todos los libros que empiecen con una letra en específico. Hint: startsWith()
+
+    Medio
+    4. Crear una función para eliminar un libro a partir del nombre.
+    5. Crear una función que regrese todos los libros en orden alfabético.
+    6. Crear una función que muestre todos los libros en mayúsculas. Hint: toUpperCase()
+
+    Difícil
+    7. Crear una función que muestre el nombre al revés de un libro en una posición N
+          0       1                 2                         3                   4
+        ["1984", "Un mundo feliz", "El fin de la eternidad", "Hábitos atómicos", "Inmune"]
+        invertirNombre(3) -> "socimóta sotibáH"
+
+        Hint: String.length ¿Cómo puedo usar un ciclo y la longitud de una cadena para invertirla?
+*/
